65871eea94ae4fe25e8b4099c653f79cc2aecfd3
mischa <rilakkuma0330k@gmail.com>
2021-04-20 18:15:23 +0800

    feat: router 4 (找出對應的路徑名稱)
    use
    - matchRoutes

--- src/Pages.js
+++ src/Pages.js
@@ -1,20 +1,56 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Switch, Route } from 'react-router-dom';
-import { renderRoutes } from 'react-router-config';
+import { Switch, Route, Link } from 'react-router-dom';
+import { renderRoutes, matchRoutes } from 'react-router-config';
 import routes from './routes';
 
 const Home = () => pug`h1.py-3 Home`;
 
 const Books = () => pug`h1.py-3 Books`;
 
-const Mobile = () => pug`h1.py-3 Mobile Phone`;
+const Mobile = (props) => {
+  console.log(props);
+  return pug`h1.py-3 Mobile Phone`
+};
 
-const Electronics = ({ route }) => {
+const Electronics = ({ route, location }) => {
+  let matchedRoutes = matchRoutes(routes, location.pathname);
+  console.log('matchedRoutes in Electronics', matchedRoutes);
+
+  matchedRoutes = [
+    {
+      route: {
+        path: '/',
+        breadcrumbName: 'Home'
+      }
+    },
+    ...matchedRoutes
+  ];
+
   return (
     <div>
       <h1 className="py-3">Electronics</h1>
+      <nav>
+        <ol>
+          {matchedRoutes.map((matchRoute, i) => {
+            const { path, breadcrumbName } = matchRoute.route;
+
+            const isActive = path === location.pathname;
+
+            return isActive ? (
+              <li key={i}>
+                {breadcrumbName}
+              </li>
+            ) : (
+              <li key={i}>
+                <Link to={path}>{breadcrumbName} </Link>
+              </li>
+            );
+          })}
+        </ol>
+      </nav>
+
       {renderRoutes(route.routes)}
     </div>
   );
@@ -22,6 +58,7 @@ const Electronics = ({ route }) => {
 
 Electronics.propTypes = {
   route: PropTypes.object,
+  location: PropTypes.object,
 }
 
 const Desktop = () => pug`h1.py-3 Desktop PC`;
